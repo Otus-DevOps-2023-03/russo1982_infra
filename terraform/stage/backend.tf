30054112518b869b97cbaf3baa0ae5fb03fd8728
Rustam V <rusyv1982@yandex.ru>
2023-06-22 16:02:32 +0300

    Update backend.tf

--- terraform/stage/backend.tf
+++ terraform/stage/backend.tf
@@ -7,8 +7,8 @@ terraform {
     bucket     = "terr-state-file"
     region     = "ru-central1"
     key        = "stage/terraform.tfstate"
-    access_key = "YCAJEPyrG1mVi"
-    secret_key = "YCOx3MIZOU4aP_xI-8i_HcuTNXHD"
+    access_key = "YCA"
+    secret_key = "YCOx3"
 
     skip_region_validation      = true
     skip_credentials_validation = true
